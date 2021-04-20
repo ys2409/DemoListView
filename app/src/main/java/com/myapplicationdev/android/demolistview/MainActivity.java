52b27d6bd431fef2620f72515b09450f34d6a974
ys2409 <19022397@myrp.edu.sg>
2021-04-20 23:27:53 +0800

    h

--- app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
@@ -3,8 +3,11 @@ package com.myapplicationdev.android.demolistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lv = (ListView) this.findViewById(R.id.lvFood);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Food selectedFood = food.get(position);
+                Toast.makeText(MainActivity.this, selectedFood.getName() + " Star: " + selectedFood.isStar(), Toast.LENGTH_LONG).show();
+            }
+        });
 
         //create a few food obj in food array
         food = new ArrayList<Food>();
